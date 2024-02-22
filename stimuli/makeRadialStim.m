00624a40c4d0f77df2fa2cdea94aa1669e8c98bc
Julien Besle <julienbesle@gmail.com>
2024-02-22 10:53:09 +0000

    makeRadialStim.m: added option to add bandpass texture within the contour and to replace bandpass contour by raised cosine contour

--- stimuli/makeRadialStim.m
+++ stimuli/makeRadialStim.m
@@ -1,4 +1,4 @@
-function pixelValue=makeRadialStim(radialHarmonics,radialAmplitudes,radialPhases, writeBitmap, axisHandle)
+function pixelValue=makeRadialStim(radialHarmonics,radialAmplitudes,radialPhases, writeBitmap, axisHandle, contourStyle, texture)
 imageDims = [256 256]; % size of the bitmap image in pixels
 imageSize = [1.75 1.75]; % size of the image in degrees of visual angle
 baseRadius = 0.5; % radius of the zeroth harmonic (base radius of the circle) in degrees of visual angle
@@ -22,6 +22,12 @@ end
 radialPhasesRad = radialPhases/180*pi; % phase of each harmonic in radians
 if ~exist('writeBitmap','var') || isempty(writeBitmap)
   writeBitmap = false; % do not write out bitmap image by default
+end
+if ~exist('contourStyle','var') || isempty(contourStyle)
+  contourStyle = 'bandpass'; % style of the contour, either 'bandpass' (as in Wilkinson & Wilson, 2002) or 'raisedCosine' (a smooth black line)
+end
+if ~exist('texture','var') || isempty(texture)
+  texture = false; % whether to add a texture within the contour
 end
 
 nHarmonics = length(radialHarmonics);
@@ -54,20 +60,47 @@ end
 % make bitmap image
 imageCenter = imageDims/2+0.5;
 sigma = 0.056; % bandpass space constant in degrees of visual angle (see eq. 2 in Wilson & Wilkinson (2002))
+beta = 0.5; % beta parameter for raised cosine (0 = cosine, 1 = square) (for raised cosines, sigma is the width parameter)
+pixelValue = zeros(imageDims);
+mask = zeros(imageDims);
 for x = 1:imageDims(1)
   for y = 1:imageDims(2)
     complexCoords = (x-imageCenter(1) + 1i*(y-imageCenter(2))) / min(imageDims/2) * min(imageSize/2);
     r = abs(complexCoords);
     phi = angle(complexCoords);
-    % get contrast value at pixel using eq. 2 from Wilson & Wilkinson (2002)
-    pixelValue(y,x) = (1 - 4*(r-radius(phi)).^2/sigma^2 + 4/3*(r - radius(phi)).^4/sigma^4 ) * exp(-(r-radius(phi)).^2/sigma^2);
-    % (x coordinates correspond to matrix colums going from left to right and
-    %  y coordinates correspond to matrix rows going from top to bottom)
+    
+    switch(contourStyle)
+      case 'bandpass'
+        % get contrast value at pixel using eq. 2 from Wilson & Wilkinson (2002)
+        pixelValue(y,x) = (1 - 4*(r-radius(phi)).^2/sigma^2 + 4/3*(r - radius(phi)).^4/sigma^4 ) * exp(-(r-radius(phi)).^2/sigma^2);
+        % (x coordinates correspond to matrix colums going from left to right and
+        %  y coordinates correspond to matrix rows going from top to bottom)
+      case 'raisedCosine'
+        % use raised cosine
+        switch(discretize( abs(r-radius(phi)), [-inf (1-beta)/2*sigma (1+beta)/2*sigma inf] ))
+          case 1
+            pixelValue(y,x) = -1;
+          case 2
+            pixelValue(y,x) = -0.5 * (1 + cos( pi/beta/sigma * (abs(r-radius(phi)) - (1-beta)/2*sigma) ) ); % raised cosine formula taken from wikipedia, where sigma = 1/T
+          case 3
+            pixelValue(y,x) = 0;
+        end
+    end
+
+    if texture && r < radius(phi)
+      mask(y,x) = 1;
+    end
+
   end
 end
 
+if texture
+  images = makebpnoise_symmetric([],1,10,12,false); % get texture bitmap
+  pixelValue = max(min( mask.*images.nz1  + pixelValue , 1),-1); % , mask texture, and overlay contour on top of texture (there's a better way to do transparency, but this will do for now)
+end
+
 % display bitmap
-if ~exist("axisHandle",'var')
+if ~exist("axisHandle",'var') || isempty(axisHandle)
     figure('name',sprintf('Harmonics = %s   A = %s   phi = %s',mat2str(radialHarmonics),mat2str(radialAmplitudes),mat2str(radialPhases)));
     axisHandle=axes;
 end
