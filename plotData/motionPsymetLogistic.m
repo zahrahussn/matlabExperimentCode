dca661f1b38d9c01b306af5735a18f661147972f
Julien Besle <julienbesle@gmail.com>
2024-05-02 16:12:24 +0100

    motionPsymetLogistic.m: now fits effect of motion adaptation

--- plotData/motionPsymetLogistic.m
+++ plotData/motionPsymetLogistic.m
@@ -1,10 +1,12 @@
 % fits logistic psychometric function to motion direction discrimination data obtained using
-% Motion/BaselineTask.py, AdatationTask.py or ImageryTask.py, and returns threshold at given performance (e.g. 99%)
+% Motion/BaselineTask.py, AdatationTask.py or ImageryTask.py
+% Logistic function is fitted to an down/up motion response function spanning the two opposite directions
 % (see: Winawer et al., 2010, A motion aftereffect from visual imagery of motion)
 % Motion discrimination is assumed to be between up (90) and down (270)
-% logistic function is fitted to an down/up motion response function spanning the two opposite direction
+% For baseline (no adaptation), returns threshold at given performance (e.g. 99%)
+% For Adaptation and Imagery, a difference in bias between the two opposite adaptation directions is modeled
 
-function [thresholdUp,thresholdDown,fit] = motionPsymetLogistic(fileName,fitBias,fitLapse)
+function [fit, thresholdUp,thresholdDown] = motionPsymetLogistic(fileName,fitBias,fitLapse)
 
 if ~exist('fileName','var') || isempty(fileName)
   fileName = "aa_pilot1_baseline.csv";
@@ -12,23 +14,39 @@ end
 if ~exist('fitBias','var') || isempty(fitBias)
   fitBias = false; % assume that there is no bias
 end
-if ~exist('fitLapse','var') || isempty(fitLapse)
-  fitLapse = true; % assume that there is no bias
-end
 
 thresholdPerf = 0.9;
 
 dat = readtable(fileName);
-trials = [dat.coherence dat.accuracy dat.direction];
+fitAdaptation = true;
+if ismember('adapt_direction',dat.Properties.VariableNames)
+  trials = [dat.coherence dat.accuracy dat.dot_direction  dat.adapt_direction];
+elseif ismember('imagery_direction',dat.Properties.VariableNames)
+  trials = [dat.coherence dat.accuracy dat.dot_direction  dat.imagery_direction];
+else
+  trials = [dat.coherence dat.accuracy dat.direction];
+  trials(:,4) = 0; % no adaptation
+  fitAdaptation = false;
+end
+
+if ~exist('fitLapse','var') || isempty(fitLapse)
+  fitLapse = ~fitAdaptation; % by default do not fit the lapse parameter for (individual) adaptation data (as in Winawer et al., 2010)
+end
 
 % make a motion response function spanning the two opposite directions
 trials(trials(:,3)==270,1) = -trials(trials(:,3)==270,1); % coherence for "down" trials is negative 
 trials(trials(:,3)==270,2) = 1-trials(trials(:,3)==270,2); % convert accuracy to probability of responding "up"
 
+%transform adaptation direction data
+trials(trials(:,4)==90,4) = 1; % 1 for up
+trials(trials(:,4)==270,4) = -1; % and -1 for down (and 0 otherwise)
+
+
+% logistic function with an additional parameter for the asymptote (lapse rate)
 scaledLogistic = @(x,p) p(3)/2 +  (1-p(3)) * 1./(1+exp(-(p(1) + p(2)*x)));
 
+% inverse logistic function (without the additional lapse parameter)
 invLogistic = @(y,p) (-p(1) - log(1./y - 1))/p(2);
-
 % derivation  of the inverse logistic function
 % logistic = @(x,p) 1./ ( 1+ exp (-(p(1) + p(2)*x)));
 % y = 1./ ( 1+ exp (-(p(1) + p(2)*x)))
@@ -39,15 +57,30 @@ invLogistic = @(y,p) (-p(1) - log(1./y - 1))/p(2);
 % p(2)*x = -p(1) - log(1./y - 1)
 % x = (-p(1) - log(1./y - 1))/p(2)
 
-% fit psychometric curve using maximum likelihood
+% logistic function with an additional parameter for the asymptote (lapse rate) and a bias parameter due to adaptation
+scaledLogistic = @(x,p,A) p(3)/2 +  (1-p(3)) * 1./(1+exp(-(p(1) + p(2)*x + p(4)*A)));
+
+% inverse logistic function (without the additional lapse parameter)
+invLogistic = @(y,p,A) (-p(1) + p(4)*A - log(1./y - 1))/p(2);
+% derivation  of the inverse logistic function
+% logistic = @(x,p) 1./ ( 1+ exp (-(p(1) + p(2)*x + p(4)*A)));
+% y = 1./ ( 1+ exp (-(p(1) + p(2)*x + p(4)*A)))
+% 1./y = 1+ exp (-(p(1) + p(2)*x + p(4)*A))
+% 1./y - 1 = exp (-(p(1) + p(2)*x + p(4)*A))
+% log(1./y - 1) = -(p(1) + p(2)*x + p(4)*A)
+% log(1./y - 1) = -p(1) - p(2)*x + p(4)*A
+% p(2)*x = -p(1) + p(4)*A - log(1./y - 1)
+% x = (-p(1) + p(4)*A - log(1./y - 1))/p(2)
+
+% Fit psychometric curve using maximum likelihood
 upResponseTrials = trials(:,2)==1;
 downResponseTrials = trials(:,2)==0;
-fiterr = @(p) -sum(log( scaledLogistic( trials(upResponseTrials,1), p ) )) -sum(log(1 - scaledLogistic( trials(downResponseTrials,1), p)));
+fiterr = @(p) -sum(log( scaledLogistic( trials(upResponseTrials,1), p, trials(upResponseTrials,4)) )) -sum(log(1 - scaledLogistic( trials(downResponseTrials,1), p, trials(downResponseTrials,4))));
 
 opt=optimset('Display','off', 'TolX',0.0001,'TolFun',0.000001, 'MaxFunEvals',1000);
 
-lowerbound = [0 -inf 0];
-upperbound = [0 inf 0];
+lowerbound = [0 -inf 0 0];
+upperbound = [0 inf 0 0];
 if fitBias
   lowerbound(1) = -1;
   upperbound(1) = 1;
@@ -55,16 +88,20 @@ end
 if fitLapse
   upperbound(3) = 1;
 end
+if fitAdaptation
+  lowerbound(4) = -inf;
+  upperbound(4) = inf;
+end
 
-init = [ mean(trials(:,1).*(trials(:,2)))*5, 5, 0]; % some  empirical initial values
+init = [ mean(trials(:,1).*(trials(:,2)))*5, 5, 0, 0]; % some  empirical initial values
 fit = fminsearchbnd(fiterr, init, lowerbound, upperbound, opt);
 
-% calculate threshold
-thresholdUp = invLogistic(thresholdPerf,fit);
-thresholdDown = invLogistic(1-thresholdPerf,fit);
-
-
-figure('name',sprintf('%s - %d%% threshold',fileName, thresholdPerf*100));
+% Plot empirical and fitted psychometric functions
+titleString = fileName;
+if ~fitAdaptation
+  titleString = sprintf('%s - %d%% threshold',titleString, thresholdPerf*100);
+end
+figure('name',titleString);
 hAxis = axes;
 hAxis.YLim = [0 1.05];
 hold on;
@@ -74,50 +111,72 @@ x= -1:0.01:1;
 plot([-1; 1],0.5*ones(2,1),'k:')
 plot(zeros(2,1),[0; 1],'k:')
 
-% plot data for each direction in different colors
+% plot data for each dot direction/adaptation direction in different colors/styles
 hAxis.YLim = [-0.05 1.05];
 hold on;
 c = 0;
-for direction = [90 270]
-  c=c+1;
+adaptDirs = unique(trials(:,4));
+markers = 'ox';
+styles = '-:';
+for iAdapt = 1:length(adaptDirs)
+  for direction = [90 270]
+    c=c+1;
+  
+    switch(direction)
+      case 90
+        colour = 'r';
+      case 270
+        colour = 'b';
+    end
+  
+    thisTrials = trials(trials(:,3) == direction & trials(:,4) == adaptDirs(iAdapt) ,:);
+    empPsyMet = emppsymet(thisTrials);
+    h(:,c) = errorbar(empPsyMet(:,1),empPsyMet(:,2),empPsyMet(:,4),[colour markers(iAdapt) styles(iAdapt)]);
 
-  switch(direction)
-    case 90
-      colour = 'r';
-    case 270
-      colour = 'b';
   end
 
-  thisTrials = trials(trials(:,3) == direction,:);
-  empPsyMet = emppsymet(thisTrials);
-  h(:,c) = errorbar(empPsyMet(:,1),empPsyMet(:,2),empPsyMet(:,4),[colour 'o']);
+  % plot model
+  plot(x,scaledLogistic(x,fit,adaptDirs(iAdapt)),'g','lineWidth',2);
 
 end
 
-% plot model
-plot(x,scaledLogistic(x,fit),'g','lineWidth',2);
+if fitAdaptation
+  thresholdUp = [];
+  thresholdDown = [];
+else
+  % calculate threshold
+  thresholdUp = invLogistic(thresholdPerf,fit,0);
+  thresholdDown = invLogistic(1-thresholdPerf,fit,0);
+  
+  % plot thresholds
+  thresholdUpPerfFit = thresholdPerf*(1-fit(3)) + fit(3)/2;
+  plot([-1;thresholdUp],thresholdUpPerfFit *ones(2,1),'g--');
+  plot(thresholdUp*ones(2,1),[-0.05 thresholdUpPerfFit],'g--');
+  text(thresholdUp,thresholdUpPerfFit-.04,sprintf('%.3f',thresholdUp),'FontSize',16);
+  
+  thresholdDownPerfFit = (1-thresholdPerf)*(1-fit(3)) + fit(3)/2;
+  plot([-1;thresholdDown],thresholdDownPerfFit *ones(2,1),'g--');
+  plot(thresholdDown*ones(2,1),[-0.05 thresholdDownPerfFit],'g--');
+  text(thresholdDown,thresholdDownPerfFit+.04,sprintf('%.3f',thresholdDown),'HorizontalAlignment','right','FontSize',16);
+end
 
-% plot thresholds
-thresholdUpPerfFit = thresholdPerf*(1-fit(3)) + fit(3)/2;
-plot([-1;thresholdUp],thresholdUpPerfFit *ones(2,1),'g--');
-plot(thresholdUp*ones(2,1),[-0.05 thresholdUpPerfFit],'g--');
-text(thresholdUp,thresholdUpPerfFit-.04,sprintf('%.3f',thresholdUp),'FontSize',16);
+if fitAdaptation
+  legend(h(1,:),{'up - adapt up','down - adapt up','up - adapt down','down - adapt down'},'location','SouthEast')
+else
+  legend(h(1,:),{'up','down'},'location','SouthEast')
+end
 
-thresholdDownPerfFit = (1-thresholdPerf)*(1-fit(3)) + fit(3)/2;
-plot([-1;thresholdDown],thresholdDownPerfFit *ones(2,1),'g--');
-plot(thresholdDown*ones(2,1),[-0.05 thresholdDownPerfFit],'g--');
-text(thresholdDown,thresholdDownPerfFit+.04,sprintf('%.3f',thresholdDown),'HorizontalAlignment','right','FontSize',16);
-
-legend(h(1,:),{'up','down'},'location','SouthEast')
-
-titleString = sprintf('slope = %.1f',fit(2));
+titleString = sprintf('Slope \\beta = %.1f',fit(2));
 if fitLapse
-  titleString = sprintf('%s, lapse = %.2f',titleString, fit(3)/2);
+  titleString = sprintf('%s, Lapse \\delta = %.2f',titleString, fit(3)/2);
 end
 if fitBias
-  titleString = sprintf('bias = %.2f, %s',fit(1), titleString);
+  titleString = sprintf('Bias \\alpha = %.2f, %s',fit(1), titleString);
 end
-title(titleString);
+if fitAdaptation
+  titleString = sprintf('%s, Adaptation bias \\gamma = %.2f',titleString, fit(4));
+end
+title(titleString,'interpreter','tex');
 
 ylabel('Probability of choosing ''Up''');
 xlabel('Coherence');
